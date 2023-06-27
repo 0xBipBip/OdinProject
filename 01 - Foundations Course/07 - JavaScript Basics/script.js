6dba9899bcca2fa2f53a868765b8611766086775
0xBipBip <oxbipbip@gmail.com>
2023-06-27 14:50:39 -0300

    part 04

---
+++ 01 - Foundations Course/07 - JavaScript Basics/script.js
@@ -0,0 +1,28 @@
+// FROM: "04 - Fundamentals Part 3"
+/* Assignment
+For now, just write each function and test the output with console.log.
+
+  - Write a function called add7 that takes one number and returns that number + 7.
+function add7(num){
+    return num + 7;
+}
+console.log(add7(5));
+  
+  - Write a function called multiply that takes 2 numbers and returns their product.
+function multiply(num1,num2){
+    return num1 * num2;
+}
+console.log(multiply(3, 2));
+
+  - Write a function called capitalize that takes a string and returns that string with only the 
+  first letter capitalized. Make sure that it can take strings that are lowercase, UPPERCASE or BoTh.
+
+function capitalize() {
+
+}
+
+- Write a function called lastLetter that takes a string and returns the very last letter of that 
+string:
+        lastLetter("abcd") should return "d"
+
+*/
